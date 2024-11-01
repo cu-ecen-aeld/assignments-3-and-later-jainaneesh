4db40a2d2631475e6b726a11f9b2aa2a739e04cc
jainaneesh <jainaneesh15@gmail.com>
2024-11-01 12:24:50 -0400

    Completed copying the necessary files, scripts and executables to home directory of rootfs

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -126,6 +126,17 @@ make CROSS_COMPILE=${CROSS_COMPILE} all
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo -e "Copying the finder related scripts and executables to rootfs home directory..."
+mkdir -p "${OUTDIR}/rootfs/home"
+mkdir -p "${OUTDIR}/rootfs/home/conf"
+cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/finder-test.sh" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/start-qemu-terminal.sh" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/start-qemu-app.sh" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/conf/assignment.txt" "${OUTDIR}/rootfs/home/conf"
+cp "${FINDER_APP_DIR}/conf/username.txt" "${OUTDIR}/rootfs/home/conf"
 
 # TODO: Chown the root directory
 
